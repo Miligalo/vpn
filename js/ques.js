5455658293971e1a5b38f7976c4dc158ca57e3bf
Miligalo <milioglo2000@gmail.com>
2022-11-01 22:00:06 +0200

    update

--- js/ques.js
+++ js/ques.js
@@ -2,7 +2,14 @@ function createQues(e) {
      let nameQues = $(e.target).parent().parent().find('.name_ques').val();
     let mainQues = `<div class="main_question card w-75 ques_style" data-name="${nameQues}">
                       <div class="ques_title">
-                            <h1>${nameQues}</h1>
+                            <h1 id="case_name">${nameQues}</h1>
+                            <div>
+                                <button type="submit" class="trash ques_trash">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </button>
+                                
+                            </div>
+                            
                          </div>
                         <div class="question_add">
                             <div class="input-group mb-3 w-50">
@@ -23,7 +30,10 @@ function createQuestion(e) {
 
     let addQuestion = `<div class="question_content" data-name="${nameQuestion}">
                             <div class="question_title">
-                            <h3>${nameQuestion}</h3>
+                                <h3 id="case">${nameQuestion}</h3>
+                                <button type="submit" class="trash question_trash">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </button>
                             </div>
                             <div class="answer_add">
                                  <div class="input-group mb-3 w-50">
@@ -36,16 +46,26 @@ function createQuestion(e) {
                         </div>`;
     $(e.target).parent().parent().parent().find('#question').append(addQuestion);
 }
+
 $(document).on('click','.create_question',function (e){
     createQuestion(e);
 });
 
 function createAnswer(e) {
+    let element_question = $(e.target).parent().parent().parent().find('#case').text();
+    let element_ques = $(e.target).parent().parent().parent().parent().parent().find('#case_name').text();
+    console.log(element_ques)
     let nameAnswer = $(e.target).parent().parent().parent().find('.answer_name').val();
     let answer = `<div class="answer_title" data-name="${nameAnswer}">
                           <label>
-                                <input type="checkbox" class="nice_checkbox" name="languages">
-                                    ${nameAnswer}
+                                Верно?
+                                    <input type="radio" name="${element_question} ${element_ques}" class="nice_checkbox" name="languages">
+                                     ${nameAnswer}
+                                    <span>
+                                         <button type="submit" class="trash answer_trash">
+                                             <i class="fa fa-trash" aria-hidden="true"></i>
+                                         </button>
+                                    </span>
                            </label>
                     </div>`;
     $(e.target).parent().parent().parent().find('#answer').append(answer);
@@ -54,19 +74,48 @@ $(document).on('click','.create_answer',function (e){
     createAnswer(e);
 });
 
+function deleteQues(e){
+    $(e.target).parent().parent().parent().parent().remove()
+}
+$(document).on('click', '.ques_trash', function (e){
+    deleteQues(e);
+});
+
+function deleteQuestion(e){
+    $(e.target).parent().parent().parent().remove()
+}
+$(document).on('click', '.question_trash', function (e){
+    deleteQuestion(e);
+});
+
+function deleteAnswer(e){
+    $(e.target).parent().parent().parent().parent().remove();
+}
+$(document).on('click', '.answer_trash', function (e){
+    deleteAnswer(e);
+})
 function save(){
-    let array = [];
-    $('.main_question').each(function (){
+
+    let ques_array = [];
+    $('.main_question').each(function (index){
         let ques_title = $(this).data('name');
-        array[ques_title] = [];
+        let question_array = [];
         $(this).find('.question_content').each(function (){
             let question_title = $(this).data('name');
-            array[ques_title][question_title] = [];
+            let answer_array = [];
             $(this).find('.answer_title').each(function (){
                 let answer_title = $(this).data('name');
-                array[ques_title][question_title][answer_title] = $(this).find('.nice_checkbox').is(":checked");
+                let answer_obj = {}
+                answer_obj[answer_title] = $(this).find('.nice_checkbox').is(":checked")
+                answer_array.push(answer_obj)
             });
+            let question_obj = {}
+            question_obj[question_title] = answer_array
+            question_array.push(question_obj);
         });
+            let ques_obj = {}
+            ques_obj[ques_title] = question_array
+            ques_array.push(ques_obj)
     });
-    console.log(array);
+    console.log(ques_array);
 }
